00d54fd0ff0b7972c2095da41e6627d0563b16f2
Naufal Aldy Pradana <blitzaldykazu@gmail.com>
2024-05-05 19:52:04 +0700

    feat: finish programs

--- src/catat/main.py
+++ src/catat/main.py
@@ -1,8 +1,8 @@
 from module.display import *
-from module.display import *
+from module.util import *
 
-income = []
-outcome = []
+incomes = []
+outcomes = []
 
 def main():
     # Show welcome menu
@@ -14,17 +14,42 @@ def main():
         menu_choice = input("Masukkan pilihan menu (1-6): \n")
         try:
             if menu_choice == "1":
-                print("1")
+                # Add income
+                try:
+                    new_income = int(input("Silahkan masukkan pemasukkan baru: \n"))
+                    input_income(incomes, new_income)
+                    
+                    for index, income in enumerate(incomes):
+                        print(f"{index} - {income}")
+                except ValueError as e:
+                    print("Input salah.  Tolong masukkan angka yang valid")
+                    continue
             elif menu_choice == "2":
-                print("2")
+                # Add outcome
+                try:
+                    new_outcome = int(input("Silahkan masukkan pengeluaran baru: \n"))
+                    input_income(outcomes, new_outcome)
+                    
+                    for index, outcome in enumerate(outcomes):
+                        print(f"{index} - {outcome}")
+                except ValueError as e:
+                    print("Input salah.  Tolong masukkan angka yang valid")
+                    continue
             elif menu_choice == "3":
-                print("3")
+                # Sum income
+                print(f"Total pemasukkan: {sum_list(incomes)}")
             elif menu_choice == "4":
-                print("4")
+                # Sum outcome
+                print(f"Total pengeluaran: {sum_list(outcomes)}")
             elif menu_choice == "5":
-                print("5")
+                # Count gap between income and outcome
+                total_incomes = sum_list(incomes)
+                total_outcomes = sum_list(outcomes)
+                
+                print(f"Jumlah pemasukkan - pengeluaran: {total_incomes-total_outcomes}")
             elif menu_choice == "6":
-                print("6")
+                # exit program
+                break
             else:
                 print("Maaf, menu tidak tersedia silahkan pilih dari nomor (1-6)")      
         except Exception as e:
---
+++ src/catat/module/util.py
@@ -0,0 +1,11 @@
+# Input money
+def input_income(list, input):
+    list.append(input)
+    return list
+
+# Sum list
+def sum_list(list):
+    sum = 0
+    for x in list:
+        sum += x
+    return sum
